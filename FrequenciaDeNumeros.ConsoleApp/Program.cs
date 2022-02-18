6eae73477945e4b0fad6d61e988f1ae5dc501c2d
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-18 16:28:31 -0300

    frequencia de numero

--- FrequenciaDeNumeros.ConsoleApp/Program.cs
+++ FrequenciaDeNumeros.ConsoleApp/Program.cs
@@ -19,11 +19,51 @@ namespace FrequenciaDeNumeros.ConsoleApp
 
             if (valor1 == valor2 || valor1 == valor3 || valor1 == valor4 || valor1 == valor5 || valor1 == valor6 || valor1 == valor7 || valor1 == valor8)
             {
+               cont++;
+            }
+            
+            if (valor2 == valor1 || valor2 == valor3 || valor2 == valor4 || valor2 == valor5 || valor2 == valor6 || valor2 == valor7 || valor2 == valor8)
+            {
                 cont++;
             }
-
+            
+            if (valor3 == valor1 || valor3 == valor2 || valor3 == valor4 || valor3 == valor5 || valor3 == valor6 || valor3 == valor7 || valor3 == valor8)
+            {
+                cont++;
+            }
+            
+            if (valor4 == valor2 || valor4 == valor3 || valor4 == valor1 || valor4 == valor5 || valor4 == valor6 || valor4 == valor7 || valor4 == valor8)
+            {
+                cont++;
+            }
+            
+            if (valor5 == valor2 || valor5 == valor3 || valor5 == valor4 || valor5 == valor1 || valor5 == valor6 || valor5 == valor7 || valor5 == valor8)
+            {
+                cont++;
+            }
+            
+            if (valor6 == valor2 || valor6 == valor3 || valor6 == valor4 || valor6 == valor5 || valor6 == valor1 || valor6 == valor7 || valor6 == valor8)
+            {
+                cont++;
+            }
+            
+            if (valor7 == valor2 || valor7 == valor3 || valor7 == valor4 || valor7 == valor5 || valor7 == valor6 || valor7 == valor1 || valor7 == valor8)
+            {
+                cont++;
+            }
+            
+            if (valor8 == valor2 || valor8 == valor3 || valor8 == valor4 || valor8  == valor5 || valor8 == valor6 || valor8 == valor7 || valor8 == valor1)
+            {
+                cont++;
+            }
             Console.WriteLine(valor1 + " aparece " + cont + " vezes");
-
+            Console.WriteLine(valor2 + " aparece " + cont + " vezes");
+            Console.WriteLine(valor3 + " aparece " + cont + " vezes");
+            Console.WriteLine(valor4 + " aparece " + cont + " vezes");
+            Console.WriteLine(valor5 + " aparece " + cont + " vezes");
+            Console.WriteLine(valor6 + " aparece " + cont + " vezes");
+            Console.WriteLine(valor7 + " aparece " + cont + " vezes");
+            Console.WriteLine(valor + " aparece " + cont + " vezes");
         }
     }
 }
