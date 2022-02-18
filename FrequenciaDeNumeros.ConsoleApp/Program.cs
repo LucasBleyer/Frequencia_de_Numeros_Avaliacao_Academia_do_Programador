22051a5c2b528f916640436b1392c180361e88a1
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-18 15:41:56 -0300

    add Frequencia de Numero

--- FrequenciaDeNumeros.ConsoleApp/Program.cs
+++ FrequenciaDeNumeros.ConsoleApp/Program.cs
@@ -6,7 +6,24 @@ namespace FrequenciaDeNumeros.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int valor1 = 7;
+            int valor2 = 8;
+            int valor3 = 10;
+            int valor4 = 8;
+            int valor5 = 260;
+            int valor6 = 4;
+            int valor7 = 10;
+            int valor8 = 10;
+
+            int cont = 1;
+
+            if (valor1 == valor2 || valor1 == valor3 || valor1 == valor4 || valor1 == valor5 || valor1 == valor6 || valor1 == valor7 || valor1 == valor8)
+            {
+                cont++;
+            }
+
+            Console.WriteLine(valor1 + " aparece " + cont + " vezes");
+
         }
     }
 }
